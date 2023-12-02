9ba9a7c54e22614781eb1900bfa39db0e7a1bf03
John Newcomb <jxnewcomb@godaddy.com>
2023-12-01 16:06:25 -0800

    Logging

--- health.py
+++ health.py
@@ -19,6 +19,7 @@ def main(now):
 
   access_token, chat_id, bot_token, user_json_string_data = get_env_vars(env_vars)
 
+  print(user_json_string_data)
   venmo = Venmo(access_token)
   telegram = Telegram(bot_token, chat_id)
   def dual_print(message):
